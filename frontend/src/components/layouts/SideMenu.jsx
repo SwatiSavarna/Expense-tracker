6d1668f67c5a86ab6a05c762aaede2def19fa023
SwatiSavarna <swatisavarna@gmail.com>
2025-07-09 13:36:36 +0530

    Fix: corrected import path for UserContext

--- frontend/src/components/layouts/SideMenu.jsx
+++ frontend/src/components/layouts/SideMenu.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { SIDE_MENU_DATA } from '../../utils/data';
-import { UserContext } from '../../context/userContext';
+import { UserContext } from '../../context/UserContext';
 import { useNavigate } from 'react-router-dom';
 import { useContext } from 'react';
 import CharAvatar from '../Cards/CharAvatar';
